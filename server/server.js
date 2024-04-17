3916867589cd4bca2a9f986d3fdc2d8dc474efb2
Jessica Hernandez <jessicamariep.hernandez@gmail.com>
2024-04-17 11:29:29 -0400

    revert cors

--- server/server.js
+++ server/server.js
@@ -23,13 +23,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.json());
-app.use(cors(
-    {
-        origin: '*',
-        methods: ['GET', 'POST', 'PATCH'],
-        credentials: true
-    }
-));
+app.use(cors());
 app.use('/checksheet', checksheetRouter);
 app.use('/schedule', scheduleRouter);
 
