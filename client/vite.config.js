c3ca8d564f1b5d58ee059333a0ca9e7050c4dc70
Jessica Hernandez <jessicamariep.hernandez@gmail.com>
2024-04-17 18:33:44 -0400

    added ist on default vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/': '/index.html'
-    }
-  }
+  root: 'src',
+  build: {
+    outDir: '../dist'
+  },
 })
+
+
+
