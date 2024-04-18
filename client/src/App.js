109a8cccd5d9db52adf8ee9e933ca648d7bf207a
Jessica Hernandez <jessicamariep.hernandez@gmail.com>
2024-04-18 18:59:01 -0400

    for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './partials/Header';
+import Footer from './partials/Footer';
+
+import ChecksheetPage from './pages/ChecksheetPage';
+import SchedulePage from './pages/SchedulePage';
+import HistoryPage from './pages/HistoryPage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ChecksheetPage />} />
+          <Route path="/schedule" element={<SchedulePage />} />
+          <Route path="/history" element={<HistoryPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
